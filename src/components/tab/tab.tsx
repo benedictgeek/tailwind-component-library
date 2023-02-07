d3482262f46bd7cbee87b26d380b9e4c1a63416b
Olushola <olushola251@gmail.com>
2023-02-07 22:27:52 +0100

    active tab style

--- src/components/tab/tab.tsx
+++ src/components/tab/tab.tsx
@@ -1,14 +1,32 @@
-import { HTMLProps, ReactNode } from "react";
+import { HTMLProps, ReactNode, useState } from "react";
 
 export interface TabsProps extends HTMLProps<HTMLDivElement> {
   items: ReactNode[];
+  defaultActiveIndex?: number;
 }
 
-export const Tabs = ({ items, ...props }: TabsProps) => {
+export const Tabs = ({
+  items,
+  defaultActiveIndex = 0,
+  ...props
+}: TabsProps) => {
+  const [activeIndex, setActiveIndex] = useState(defaultActiveIndex);
   return (
     <div className={`flex overflow-scroll w-full`} {...props}>
-      {items.map((item) => {
-        return <div className={`mx-0 my-7 px-7 py-2`}>{item}</div>;
+      {items.map((item, index) => {
+        const isActiveTab = index === activeIndex;
+        return (
+          <div
+            className={`mx-0 my-7 px-7 py-2 whitespace-nowrap cursor-pointer transition-colors ${
+              isActiveTab ? "border-b-2 border-black" : ""
+            }`}
+            onClick={() => {
+              setActiveIndex(index);
+            }}
+          >
+            {item}
+          </div>
+        );
       })}
     </div>
   );
