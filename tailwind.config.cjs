7016760c530a66ba67fd81ad8b64c879a6eb1f41
Olushola <olushola251@gmail.com>
2023-02-12 16:37:36 +0100

    setting up the build process for the library

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,7 @@ module.exports = {
     "./index.html",
     "./.storybook/preview-head.html",
     "./src/**/*.{js,ts,tsx,jsx}",
+    "./node_modules/tailwind-component-library/**/*.{js,ts}"
   ],
   theme: {
     extend: {},
--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,12 @@ import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    //  dts()
-  ],
+  plugins: [react(), dts()],
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.tsx"),
       name: "tailwind_component_library",
     },
+    copyPublicDir: true,
   },
 });
