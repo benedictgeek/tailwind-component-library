403965b166264f7d2164c34e230f3fda98ad9e91
Olushola <olushola251@gmail.com>
2023-02-20 06:36:52 +0100

    trying a new font family

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,10 +5,18 @@ module.exports = {
     "./.storybook/preview-head.html",
     "./src/**/*.{js,ts,tsx,jsx}",
     "./pages/**/*.{js,ts,tsx,jsx}",
-    "./node_modules/tailwind-component-library/**/*.{js,ts}"
+    "./node_modules/tailwind-component-library/**/*.{js,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryColor: "#ff7043",
+        assentColor: "#2e66f6",
+        grey: "#78889b",
+        black: "#263238",
+      },
+      // fontFamily: { sans: ["Strawford", "sans-serif"] },
+    },
   },
   plugins: [],
 };
